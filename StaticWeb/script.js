15f7f7c81a57e2b0f0d05ea6f606c855a6f03011
SaadiMahdi <saadimahdi2002@gmail.com>
2022-04-23 21:27:25 +0100

    final changes

--- StaticWeb/script.js
+++ StaticWeb/script.js
@@ -1,11 +1,34 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({ items: 4, nav: true });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
   });
 
 const navbar ={
     button: document.querySelector('.main-nav ul .button'),
     nav: document.querySelector('.main-nav'),
+    bars: document.querySelector('.main-nav .Menu'),
+    elements: document.querySelector('.main-nav .main-nav-elements'),
 };
+
+const regbutton =  document.querySelector('.about-sub .button');
 
 const modal = document.querySelector('.modal');
 
@@ -16,8 +39,16 @@ const schedule ={
     days: document.querySelectorAll('.schedule-day'),
 };
 
+const MediaQuery = window.matchMedia('(max-width: 800px)');
+
+
+
+
 function showModal(){
     navbar.button.onclick = function(){
+        modal.classList.add('open');
+    };
+    regbutton.onclick = function(){
         modal.classList.add('open');
     };
 };
@@ -31,6 +62,19 @@ function closeModal(){
 }
 
 closeModal();
+
+
+function responsiveNav() {
+    navbar.bars.onclick = function(){
+        
+        if(navbar.elements.classList.contains("show")){
+            navbar.elements.classList.remove("show");
+        }else{
+            navbar.elements.classList.add("show");
+        }
+    };
+}
+responsiveNav();
 
 
 function HideDays() {
